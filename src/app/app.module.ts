9a078529feaf793c8169251b12bb6fec87b54fd3
wesley.castelani@hotmail.com <wesley.castelani@hotmail.com>
2023-06-28 20:24:15 -0300

    aprendendo sobre templates e paginas dinamicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeuPrimeiroCompoment } from './meu-primeiro/meu-primeiro.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuPrimeiroCompoment,
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
